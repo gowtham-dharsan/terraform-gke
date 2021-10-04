bec6ac19bb16156c0764260a2be495fb615d6bd0
gowtham-dharsan <76205623+gowtham-dharsan@users.noreply.github.com>
2021-10-04 07:53:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,39 @@ resource "google_container_cluster" "default" {
   description = "Demo GKE Cluster"
   location    = var.location
 
-  remove_default_node_pool = true
+  remove_default_node_pool = false
   initial_node_count       = var.initial_node_count
-
+  
+  release_channel {
+    channel = "REGULAR"
+    # RAPID, REGULAR, STABLE & UNSPECIFIED - Not setting, take default
+  }
+  
+  enable_shielded_nodes = true
+  enable_binary_authorization = false
+  
+  network policy {
+    provider = PROVIDER_UNSPECIFIED
+    enabled = false
+  }
+  
+  addons_config{
+    http_load_balancing {
+      disabled = false 
+    }
+    horizontal_pod_autoscaling {
+      disabled = true
+    }
+    gce_persistent_disk_csi_driver_config {
+      enabled = true
+    }
+  }
+  logging_service = "none"
+  monitoring_service = "none"
+  
+  
+  
+  
   master_auth {
     username = ""
     password = ""
